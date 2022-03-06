d34b8068cd0dfdd084394c8c1d879951409f2e24
xopenwrt <98641952+xopenwrt@users.noreply.github.com>
2022-03-07 00:37:40 +0800

    fix passwall

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -76,6 +76,7 @@ Firmware_Diy() {
 		;;
 		x86_64)
 			AddPackage git other openwrt-passwall xiaorouji packages
+			AddPackage git other openwrt-packages kenzok8 master
 			rm -rf packages/lean/autocore
 			AddPackage git lean autocore-modify Hyy2001X master
 		;;
