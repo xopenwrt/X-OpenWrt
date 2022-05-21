e458cbcb0d2896f2c3035c1043a7e5045a4eb935
xopenwrt <98641952+xopenwrt@users.noreply.github.com>
2022-05-22 00:17:00 +0800

    change cp to mv

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -346,7 +346,7 @@ Firmware_Diy_End() {
 	esac
 	[[ $(ls) =~ 'AutoBuild-' ]] && {
 		cd -
-		cp -a ${Firmware_Path}/AutoBuild-* bin/Firmware
+		mv -a ${Firmware_Path}/AutoBuild-* bin/Firmware
 	}
 	echo "[$(date "+%H:%M:%S")] Actions Avaliable: $(df -h | grep "/dev/root" | awk '{printf $4}')"
 	ECHO "[Firmware_Diy_End] Done"
